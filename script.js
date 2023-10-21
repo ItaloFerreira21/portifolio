aed7278d8b7faad25de011f1ba81616eb5399ea8
ItaloFerreira21 <134016682+ItaloFerreira21@users.noreply.github.com>
2023-10-21 12:18:21 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode (){
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  const img = document.querySelector ("#profile img")
+  if(html.classList.contains('light')) {
+    img.setAttribute('src','./assets/ft22.jpg')
+  } else {
+  img.setAttribute('src','./assets/euu.jpg')
+  }
+}
